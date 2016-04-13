d06801a0f8ab8f81681874db7810b8c6c52ede48
Hasan Hameed <teflanzy@gmail.com>
2016-04-14 00:05:31 +0300

    add UBER aarm-eabi-4.8 toolchain

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 export ARCH=arm
-export CROSS_COMPILE=/opt/toolchains/arm-eabi-4.7/bin/arm-eabi-
+export CROSS_COMPILE=/home/teflanzy/Desktop/android/kernel/toolchains/uber-arm-eabi-4.8/bin/arm-eabi-
 mkdir output
 
 make -C $(pwd) O=output VARIANT_DEFCONFIG=msm8974_sec_viennalteeur_defconfig msm8974_sec_defconfig SELINUX_DEFCONFIG=selinux_defconfig
